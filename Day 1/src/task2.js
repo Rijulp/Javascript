612420c0df11236e7ec2ce90439f226a3fc37f73
Rijul <89975105+Rijulp@users.noreply.github.com>
2024-05-02 11:48:28 +0530

    Add files via upload

---
+++ Day 1/src/task2.js
@@ -0,0 +1,44 @@
+//Day 1 Task 2
+// Write a javascript function to create a copy of an object. 
+// The function will take input and an object and return a copy of the same object
+// The copy should take care of any common references 
+let userDetails = {
+    name:"hary",
+    age:20,
+    gender:"male",
+    entertainment:"netflix",
+    activities :{
+        sports:"football",
+        cooking:{
+            rice:"plain",
+            soup:"tomato",
+        },
+    }
+}
+
+
+let randomdetails = userDetails;
+randomdetails.address = "global city";
+//1 spread operator
+let ud1 = {...userDetails}
+
+//2 object.assign
+let randomDetails2 = Object.assign({},userDetails);
+randomDetails2.name = "Raj";
+
+
+// json.parse stringify
+let randomdetails3 = JSON.parse(JSON.stringify(userDetails));
+randomdetails3.name = "akash";
+randomdetails3.activities.cooking.rice = "fried";
+
+
+//copy check
+let copyCheck = userDetails === randomdetails;
+console.log(copyCheck);
+let copyCheck2 = ud1 === randomdetails3;
+console.log(copyCheck2);
+
+//log
+console.log(randomDetails2);
+console.log(randomdetails3);
